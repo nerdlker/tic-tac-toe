c0a7085aab413bcbb5078b21586c36978fd8d887
nerdlekar <nerdlekar@gmail.com>
2023-12-13 21:28:15 +0530

    version 1.1

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 Tic tac toe game
 
-Version 1 playable at https://nerdlker.github.io/tic-tac-toe/
+Version 1.1 - playable at https://nerdlker.github.io/tic-tac-toe/
+
+- Refactored functions to be within objects.
+- fixed a bug where n would show losing player as winner
+- fixed game showing as draw instead of win on last move
+- removed reset button and made the `new game` button contextual.
+- updated css to be slightly easier on the eyes.
+
+<img width="588" alt="tictactoe_v2" src="https://github.com/nerdlker/tic-tac-toe/assets/63708203/14e7c1aa-8021-4c18-9a36-168700d4d8e8">
+
+Version 1
+
+- Basic CSS/HTML UI
+- Got the tic tac toe logic working.
+  <img width="573" alt="tictactoe_v1" src="https://github.com/nerdlker/tic-tac-toe/assets/63708203/feb45e21-1717-495a-81d1-9c9185d30583">
 
 # todo
 
 - Get a better color scheme in CSS
 - Add Hover functionality
-- Refactor code to make everything into objects
 - Implement Win Animaton
-- Reset button is redundant. Need to fix that.
--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const GameBoard = (function () {
   //DOM elements
   const cells = document.querySelectorAll(".cell");
-  const resettBtn = document.getElementById("reset");
+  // const resettBtn = document.getElementById("reset");
   const startGameBtn = document.getElementById("new-game");
   const gameStatus = document.getElementById("status");
   let spaces = Array(9).fill(null);
+  var currentPlayer;
 
   const Gamestate = {
     winCombos: [
@@ -20,14 +21,16 @@ const GameBoard = (function () {
     resetGame: () => {
       cells.forEach((cell) => (cell.innerHTML = ""));
       spaces = Array(9).fill(null);
-      currentPlayer = playerOne;
-      displayController.resetGame();
     },
     startGame: () => {
       Gamestate.resetGame();
+      currentPlayer = playerOne;
       cells.forEach((cell) => cell.addEventListener("click", Gamestate.gamePlay));
       displayController.startGame();
-      console.log("i was clicked");
+    },
+
+    stopGame: () => {
+      cells.forEach((cell) => cell.removeEventListener("click", Gamestate.gamePlay));
     },
 
     gamePlay: (e) => {
@@ -35,38 +38,44 @@ const GameBoard = (function () {
       if (!spaces[id]) {
         spaces[id] = currentPlayer;
         e.target.innerText = currentPlayer.mark;
-        // currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+        if (checkForWin() || checkForDraw()) {
+          console.log("game stop");
+        } else {
+          currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
+          displayController.turn();
+        }
 
-        displayController.turn();
         checkForWin();
         checkForDraw();
-        currentPlayer = currentPlayer == playerOne ? playerTwo : playerOne;
 
         function checkForWin() {
           for (const combo of Gamestate.winCombos) {
             const [a, b, c] = combo;
             if (spaces[a] && spaces[a] === spaces[b] && spaces[a] === spaces[c]) {
               winConditon();
+              return true;
             }
           }
           return false; // No winner yet
         }
+
         function checkForDraw() {
           if (spaces.every((space) => space !== null) && !checkForWin()) {
             console.log("It's a draw!");
             displayController.draw();
+            Gamestate.stopGame();
+            return true;
           }
+          return false; // Not Draw
         }
       }
       function winConditon() {
-        console.log(currentPlayer.name, "wins");
-        cells.forEach((cell) => cell.removeEventListener("click", Gamestate.gamePlay));
-        gameStatus.textContent = `${currentPlayer.name} Wins!`;
+        Gamestate.stopGame();
+        displayController.win();
       }
     },
   };
 
-  resettBtn.addEventListener("click", Gamestate.resetGame);
   startGameBtn.addEventListener("click", Gamestate.startGame);
 
   //Player Factory
@@ -77,24 +86,27 @@ const GameBoard = (function () {
   //Creates player objects
   const playerOne = player("Anish", "X", false);
   const playerTwo = player("Shilpa", "O", false);
-  var currentPlayer;
 
   const displayController = {
     startGame: function () {
-      this.turn();
+      gameStatus.textContent = `${currentPlayer.name}'s Turn with ${currentPlayer.mark}`;
+      startGameBtn.textContent = "Reset";
     },
 
-    resetGame: function () {
-      gameStatus.textContent = "Board has been reset, Click 'New Game' to Start";
-    },
     turn: function () {
       gameStatus.textContent = `${currentPlayer.name}'s Turn with ${currentPlayer.mark}`;
+    },
+    win: () => {
+      gameStatus.textContent = `${currentPlayer.name}'s Wins`;
+      console.log("win");
+      startGameBtn.textContent = "Play Again?";
     },
 
     draw: () => {
       gameStatus.textContent = `Game is a Draw`;
+      startGameBtn.textContent = "Play Again?";
     },
   };
 
-  // return { playerOne, playerTwo, spaces, currentPlayer };
+  return { playerOne, playerTwo, spaces, currentPlayer };
 })();
